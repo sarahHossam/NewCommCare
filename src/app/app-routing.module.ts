e466b0d8139ccbd8d01a9792804657744afa8660
sarahHossam <sarahossam50@gmail.com>
2018-05-29 13:27:27 -0700

    routing 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { DoctorDetailsComponent } from './doctors/doctor-details/doctor-details.
 import { DoctorListingsComponent } from './doctors/doctor-listings/doctor-listings.component';
 import { DoctorItemSmallComponent } from './doctors/doctor-item-small/doctor-item-small.component';
 import { AppComponent } from './app.component';
-
-
+import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot([{ path:'', component: AppComponent}])
+    HomeModule,
+    RouterModule.forRoot([
+      { path:'home', component: HomeComponent},
+      { path:'', redirectTo:'/home', pathMatch:'full'},
+    ])
   ],
   declarations: [],
   exports:[ RouterModule]
