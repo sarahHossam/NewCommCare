c913ab44dc0b0ffdb96738165d039320274b1f8c
sarahHossam <sarahossam50@gmail.com>
2018-05-30 14:28:55 -0700

    ahgsjsb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,27 @@
 import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DoctorDetailsComponent } from './doctors/doctor-details/doctor-details.component';
-import { DoctorListingsComponent } from './doctors/doctor-listings/doctor-listings.component';
-import { DoctorItemSmallComponent } from './doctors/doctor-item-small/doctor-item-small.component';
 import { AppComponent } from './app.component';
 import { DepartmentsComponent } from './departments/departments.component';
 import { HospitalHomeModule } from 'src/app/hospital-home/hospital-home.module';
 import { HospitalHomeComponent } from 'src/app/hospital-home/hospital-home.component';
 import { DoctorsModule } from './doctors/doctors.module';
+import { DepartmentsModule } from './departments/departments.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    HospitalHomeComponent,
+    DepartmentsModule,
+    DoctorsModule,
     RouterModule.forRoot([
       { path:'home', component: HospitalHomeComponent},
       { path:'', redirectTo:'/home', pathMatch:'full'}
 
     ])
   ],
-  declarations: [],
+  declarations: [HospitalHomeComponent],
   exports:[ RouterModule]
 
 })
