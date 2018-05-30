723b1bbfd5784d7f386e34959243107a144bdc60
sarahHossam <sarahossam50@gmail.com>
2018-05-30 12:31:18 -0700

    nmxnz

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HospitalHeaderComponent } from './hospital-header/hospital-header.component';
-/* import { HospitalComponent } from './hospital/hospital.component'; */
-import { HospitalFooterComponent } from 'src/app/shared/hospital-footer/hospital-footer.component';
-import { MenuComponent } from './menu/menu.component';
-import { RouterModule } from '@angular/router';
-import { DepartmentsModule } from '../departments/departments.module';
-import { DoctorsModule } from '../doctors/doctors.module';
-import { HospitalAboutUsComponent } from './hospital-About-Us/hospital-About-Us.component';
-import { HospitalLoginComponent } from './hospital-login/hospital-login.component';
-
-@NgModule({
-  imports: [
-    CommonModule,
-    DepartmentsModule,
-    RouterModule,
-    DoctorsModule
-  ],
-  declarations: [
-    MenuComponent,
-    HospitalHeaderComponent, 
-    HospitalFooterComponent,    ,
-    HospitalAboutUsComponent,
-    HospitalLoginComponent
-],
-exports:
-[
-  HospitalHeaderComponent, 
-  HospitalFooterComponent,
-]
-})
-export class SharedModule { }
-=======
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HospitalHeaderComponent } from './hospital-header/hospital-header.component';
@@ -65,4 +30,3 @@ exports:
 ]
 })
 export class SharedModule { }
->>>>>>> 52047956454f57885c30ea03bafa6bc88161279d
